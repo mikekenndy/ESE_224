e7680eb7eee001ef5840b53aac9046c3f5464a3a
mikekenndy <Mike.Kennedy.Dev@gmail.com>
2019-10-11 20:08:06 -0400

    Starting homework 03

---
+++ Homework_03/vector_sort.cpp
@@ -0,0 +1,26 @@
+// Mike Kennedy - 108715992
+// ESE224 - Homework 03
+// Problem 01 Sorting a vector
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to sort a vector of unknown size in
+//  DESCENDING order, returns the sorted function
+vector<int> v()
+{
+  vector<int> vec;
+  vec.push_back(10);
+
+  return vec;
+}
+
+int main()
+{
+  vector<int> test;
+  test = v();
+  
+  return 0;
+}
